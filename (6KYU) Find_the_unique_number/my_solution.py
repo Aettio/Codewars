cabae058e58755efdf7a5469dcd8caaf89be7923
Roman <61010440+Aettio@users.noreply.github.com>
2021-02-08 21:17:21 +0200

    Update my_solution.py

--- (6KYU) Find_the_unique_number/my_solution.py
+++ (6KYU) Find_the_unique_number/my_solution.py
@@ -1,10 +1,3 @@
-def bouncing_ball(h, bounce, window):
-    if h != 0 and bounce > 0 and bounce < 1 and window < h:
-        num_times = 1
-        h *= bounce
-        while h > window: 
-            h *= bounce
-            num_times += 2
-        return num_times
-    
-    else: return -1 
+def find_uniq(arr):
+    a, b = set(arr)
+    return a if arr.count(a) == 1 else b 
